a57158298ff129bb850a4e9db600d8bdc546a0a8
bane debeljevic <bane.debeljevic@ontario.ca>
2020-07-03 13:38:26 -0400

    Adjusting the analytics code

--- src/shared/analytics.js
+++ src/shared/analytics.js
@@ -4,7 +4,7 @@ export default ({ courthouse, lang, outcome }) => {
       event: "covid-19-cs-outcome",
       outcome,
       lang,
-      courthouse: courthouse[`court_name_display${lang === "fr" ? "_fr" : ""}`],
+      courthouse: courthouse.court_name_display,
     })
   }
 }
