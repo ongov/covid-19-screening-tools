6f4ce4d83b48f274a0bc87e616875183075d65bb
Jennifer Lee <Jennifer.Lee2@ontario.ca>
2020-09-04 12:39:59 -0400

    updates the who are you question title. updates the look of the landing page content

--- src/pages/school-screening/context.js
+++ src/pages/school-screening/context.js
@@ -3,7 +3,7 @@ import React from "react"
 import ContextTemplate from "../../templates/context-template-school"
 
 const Context = () => (
-  <ContextTemplate lang="en" questionId="q8" question="Who are you?" title="I am doing this screening as a:" />
+  <ContextTemplate lang="en" questionId="q8" question="I am doing this screening as a:" />
 )
 
 export default Context
