560faab8783bc1136d37c4ad21613fab1453fbb7
bane debeljevic <bane.debeljevic@ontario.ca>
2020-09-04 14:03:10 -0400

    Adding global state hook to the symptoms template to negate the need to use magic strings

--- src/templates/symptoms-template.js
+++ src/templates/symptoms-template.js
@@ -6,37 +6,33 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Question from "../components/question"
 import ContinueButton from "../components/continue-button"
-import { GlobalStateContext, GlobalDispatchContext } from "../context/global-context-provider"
+import { GlobalDispatchContext } from "../context/global-context-provider"
 import Checkbox from "../components/checkbox"
 import { general, symptomsTitle, symptoms } from "../localized_content"
-import { logic, questions } from "../shared"
+import { logic, questions, useGlobalState } from "../shared"
 
-const SymptomsTemplate = ({ lang, screenerType }) => {
-  const state = useContext(GlobalStateContext)
+const SymptomsTemplate = () => {
   const dispatch = useContext(GlobalDispatchContext)
+  const { isGuardian, state, state: { language, screener_type: screenerType, q1 } = {} } = useGlobalState()
 
   function handleContinueClick() {
     dispatch({ type: "SYMPTOMS_CONTINUE_CLICKED" })
-    navigate(`${general[lang][screenerType].basePath}${questions[logic[screenerType].q1.cont(state)][lang]}`)
+    navigate(`${general[language][screenerType].basePath}${questions[logic[screenerType].q1.cont(state)][language]}`)
   }
 
   return (
-    <Layout lang={lang} screenerType={screenerType}>
-      <SEO lang={lang} />
+    <Layout lang={language} screenerType={screenerType}>
+      <SEO lang={language} />
       <SkipNavContent>
         <Question
-          lang={lang}
-          title={general[lang][screenerType].title}
-          buttons={<ContinueButton lang={lang} clickHandler={handleContinueClick} />}
+          lang={language}
+          title={general[language][screenerType].title}
+          buttons={<ContinueButton lang={language} clickHandler={handleContinueClick} />}
         >
           <div className="ontario-form-group">
             <fieldset className="ontario-fieldset">
-              <legend>
-                {state.screenie && state.screenie === "guardian"
-                  ? symptomsTitle[lang].alt_text
-                  : symptomsTitle[lang].text}
-              </legend>
-              <p>{symptomsTitle[lang].sup_text}</p>
+              <legend>{isGuardian ? symptomsTitle[language].alt_text : symptomsTitle[language].text}</legend>
+              <p>{symptomsTitle[language].sup_text}</p>
               <div className="ontario-question__options">
                 <div className="ontario-checkboxes">
                   {Object.keys(symptoms).map(id => (
@@ -44,10 +40,10 @@ const SymptomsTemplate = ({ lang, screenerType }) => {
                       key={id}
                       id={id}
                       value={id}
-                      text={symptoms[id][lang].text}
+                      text={symptoms[id][language].text}
                       labelStyle="--large"
-                      supplementaryText={symptoms[id][lang].sup_text || ""}
-                      checkboxState={state.q1 && state.q1.hasOwnProperty(id)}
+                      supplementaryText={symptoms[id][language].sup_text || ""}
+                      checkboxState={q1 && q1.hasOwnProperty(id)}
                       checkboxOnChange={() => dispatch({ type: "TOGGLE_SYMPTOM", id })}
                     />
                   ))}
