5919c6278f54b263f859e5cdaf82ce6eae720c89
Jennifer Lee <Jennifer.Lee2@ontario.ca>
2020-09-04 21:09:51 -0400

    Changes auto-isoler to isoler

--- src/pages/depistage-scolaire/isolement.js
+++ src/pages/depistage-scolaire/isolement.js
@@ -16,9 +16,9 @@ const LautoIsolement = () => {
     noLink={logic[screenerType].q3.no}
   >
       {state.screenie && state.screenie === "guardian" ? (
-          <h2>Un médecin, fournisseur de soins de santé ou bureau de santé publique leur a-t-il dit qu’ils devraient actuellement s’auto-isoler (en restant à la maison)? </h2>
+          <h2>Un médecin, fournisseur de soins de santé ou bureau de santé publique leur a-t-il dit qu’ils devraient actuellement s'isoler (en restant à la maison)? </h2>
     ) : (
-          <h2>Un médecin, fournisseur de soins de santé ou bureau de santé publique vous a-t-il dit que vous devriez actuellement vous auto-isoler (en restant à la maison)? </h2>
+          <h2>Un médecin, fournisseur de soins de santé ou bureau de santé publique vous a-t-il dit que vous devriez actuellement vous isoler (en restant à la maison)? </h2>
          )}
     </QuestionTemplate>
   )
