e5959665b3861798f2c27339f1c3d2cd3782a6f3
bane debeljevic <bane.debeljevic@ontario.ca>
2020-09-10 11:39:48 -0400

    Removing the school and school board selection from the school screener

--- src/templates/school/landing-page-template.js
+++ src/templates/school/landing-page-template.js
@@ -8,19 +8,14 @@ import { SkipNavContent } from "@reach/skip-nav"
 import SEO from "../../components/seo"
 import Layout from "../../components/layout"
 import Button from "../../components/button"
-import Checkbox from "../../components/checkbox"
-import AutocompleteDropdown from "../../components/autocomplete-dropdown"
 
-import { GlobalDispatchContext, GlobalStateContext } from "../../context/global-context-provider"
-import { questions, replaceSchoolBoardAcronyms } from "../../shared"
-import { general, schoolLanding, formatDate, getLocalizedSchoolDataField } from "../../localized_content"
+import { GlobalDispatchContext } from "../../context/global-context-provider"
+import { questions } from "../../shared"
+import { general, schoolLanding, formatDate } from "../../localized_content"
 
-import schools from "../../data/schools.json"
-import schools_fr from "../../data/schools_fr.json"
-
-const CenteredDiv = styled.div`
+const StartScreeningCtaContainer = styled.div`
   display: block;
-  margin: 0 auto;
+  margin: 4rem auto 5rem;
   button {
     display: block;
     margin: 1rem auto 1.75rem;
@@ -40,45 +35,13 @@ export default ({ lang }) => {
     }
   `)
 
-  const localizedSchools = lang === "en" ? schools : schools_fr
-
-  const localizedSchoolBoards = Array.from(
-    new Set(localizedSchools.map(school => school[getLocalizedSchoolDataField(lang, "Board Name")]))
-  ).map(item => ({ label: replaceSchoolBoardAcronyms(item), value: item }))
-
-  const [schoolBoard, setSchoolBoard] = useState()
-  const [notFound, setNotFound] = useState(false)
-  const [boardSelectError, setBoardSelectError] = useState()
-  const [schoolSelectError, setSchoolSelectError] = useState()
-
   const dispatch = useContext(GlobalDispatchContext)
-  const state = useContext(GlobalStateContext)
-
-  const SchoolOptions = props => (
-    <components.Option {...props}>
-      <span>
-        <strong>{props.label}</strong>
-      </span>
-      <br />
-      <span>{props.value[getLocalizedSchoolDataField(lang, "Street")]}</span>
-    </components.Option>
-  )
 
   const [screeningInfoStart, screeningInfoEnd] = schoolLanding[lang].screeningInfo.split(
     `[${schoolLanding[lang].linkText}]`
   )
 
   const handleClick = () => {
-    if (!schoolBoard && !notFound) {
-      setBoardSelectError(true)
-      return
-    }
-
-    if (!state.school && !notFound) {
-      setSchoolSelectError(true)
-      return
-    }
-
     dispatch({ type: "CS_START", screener_type: "school", language: lang })
     navigate(`${general[lang][screenerType].basePath}${questions.q8[lang]}`)
   }
@@ -117,83 +80,10 @@ export default ({ lang }) => {
               </a>
               {screeningInfoEnd}
             </p>
-            <AutocompleteDropdown
-              tabOrder="1"
-              selectOptions={localizedSchoolBoards}
-              selectValue={schoolBoard}
-              selectId="boards"
-              selectTitle={schoolLanding[lang].boardSelect}
-              onSelectChange={option => {
-                setBoardSelectError(false)
-                setSchoolBoard(option)
-                dispatch({
-                  type: "SCHOOL_SELECTED",
-                  school: null,
-                })
-              }}
-              selectError={boardSelectError}
-              selectErrorMessage={schoolLanding[lang].boardSelectError}
-              placeholerText={schoolLanding[lang].placeholderText}
-              noOptionsText={schoolLanding[lang].noOptionsText}
-            />
-            {schoolBoard && (
-              <AutocompleteDropdown
-                tabOrder="2"
-                autoFocus={true}
-                selectOptions={localizedSchools
-                  .filter(school => school[getLocalizedSchoolDataField(lang, "Board Name")] === schoolBoard.value)
-                  .map(school => ({
-                    label: `${school[getLocalizedSchoolDataField(lang, "School Name")]} - ${
-                      school[getLocalizedSchoolDataField(lang, "City")]
-                    }`,
-                    value: school,
-                  }))}
-                selectValue={state.school}
-                selectId="schools"
-                selectTitle={schoolLanding[lang].schoolSelect}
-                selectOptionComponent={SchoolOptions}
-                onSelectChange={option => {
-                  setSchoolSelectError(false)
-                  dispatch({
-                    type: "SCHOOL_SELECTED",
-                    school: option,
-                  })
-                  setNotFound(false)
-                }}
-                selectError={schoolSelectError}
-                selectErrorMessage={schoolLanding[lang].schoolSelectError}
-                placeholerText={schoolLanding[lang].placeholderText}
-                noOptionsText={schoolLanding[lang].noOptionsText}
-              />
-            )}
-            <div className="ontario-checkboxes__not-found-container">
-              <Checkbox
-                tabOrder="3"
-                id="not-found"
-                value={notFound}
-                checkboxState={notFound}
-                text={schoolLanding[lang].cantFindSchoolLabel}
-                labelStyle="--large"
-                checkboxOnChange={e => {
-                  if (!e.target.checked) {
-                    setNotFound(false)
-                    return
-                  }
 
-                  setBoardSelectError(false)
-                  setSchoolSelectError(false)
-                  setNotFound(true)
-                  setSchoolBoard("")
-                  dispatch({
-                    type: "SCHOOL_SELECTED",
-                    school: null,
-                  })
-                }}
-              />
-            </div>
-            <CenteredDiv>
+            <StartScreeningCtaContainer>
               <Button tabOrder="4" text={schoolLanding[lang].button} clickHandler={handleClick} />
-            </CenteredDiv>
+            </StartScreeningCtaContainer>
           </div>
         </div>
       </SkipNavContent>
