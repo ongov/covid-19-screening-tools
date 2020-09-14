a2eb85ed37f1f111fa8bfc53ee644ce89e95f218
bane debeljevic <bane.debeljevic@ontario.ca>
2020-09-14 16:17:15 -0400

    Adding a question mark to FR feedback question

--- src/localized_content/feedback.js
+++ src/localized_content/feedback.js
@@ -8,7 +8,7 @@ const feedback = {
   fr: {
     title: "Commentaires ",
     content: "Faites-nous part de vos impressions sur le dépistage de la COVID-19 pour les écoles aujourd’hui.",
-    assessment_centres_content: "Que pensez-vous de l’outil de dépistage pour les écoles.",
+    assessment_centres_content: "Que pensez-vous de l’outil de dépistage pour les écoles?",
     link: "https://covid-19.ontario.ca/fr/commentaires/depistage-pour-les-ecoles",
   },
 }
