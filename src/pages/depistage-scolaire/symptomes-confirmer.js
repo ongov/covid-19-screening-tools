aa67c1af71f956451438df90dd53abe6174aa921
Jennifer Lee <Jennifer.Lee2@ontario.ca>
2020-09-14 14:07:42 -0400

    Adds text to the confirm symptoms question

--- src/pages/depistage-scolaire/symptomes-confirmer.js
+++ src/pages/depistage-scolaire/symptomes-confirmer.js
@@ -16,9 +16,15 @@ const SymptomsConfirm = () => {
     noLink={logic[screenerType].q9.no}
       >
       {state.screenie && state.screenie === "guardian" ? (
+            <>
             <h2>Leurs symptômes sont-ils normaux pour eux (en lien avec une cause ou un problème de santé connus)? </h2>
+            <p>Ils devraient parler à un médecin s’ils se sentent malade ou s’ils pensent avoir un rhume ou une grippe, car les symptômes sont similaires à ceux de la COVID-19.</p>
+            </>
         ) : (
+            <>
             <h2>Vos symptômes sont-ils normaux pour vous (en lien avec une cause ou un problème de santé connus)?</h2>
+            <p>Vous devriez parler à un médecin si vous vous sentez malade ou si vous pensez avoir un rhume ou une grippe, car les symptômes sont similaires à ceux de la COVID-19.</p>
+            </>
         )}
     </QuestionTemplate>
 )
