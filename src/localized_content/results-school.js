26abc8569dc68c850a751a91eaf3b147280de540
Jennifer Lee <Jennifer.Lee2@ontario.ca>
2020-09-14 11:12:44 -0400

    Update to the denied screen content, under Going to School

--- src/localized_content/results-school.js
+++ src/localized_content/results-school.js
@@ -107,7 +107,7 @@ const resultsSchool = {
           <li>it has been at least 24 hours since your symptoms started improving (if you had symptoms)</li>
         </ul>
         <h3 className="h4-style">If you test negative (you do not have the virus)</h3>
-        <p>You can return to school if all the following apply:</p>
+        <p>You should isolate for 14 days unless all the following apply:</p>
         <ul>
           <li>you do not have a fever (without using medication)</li>
           <li>it has been at least 24 hours since your symptoms started improving (if you had symptoms)</li>
@@ -130,7 +130,7 @@ const resultsSchool = {
           <li>it has been at least 24 hours since their symptoms started improving (if they had symptoms) </li>
         </ul>
         <h3 className="h4-style">If they test negative (they do not have the virus)</h3>
-        <p>They can return to school if all the following apply:</p>
+        <p>They should isolate for 14 days, unless  all the following apply:</p>
         <ul>
           <li>they do not have a fever (without using medication)</li>
           <li>it has been at least 24 hours since their symptoms started improving (if they had symptoms)</li>
@@ -258,7 +258,7 @@ const resultsSchool = {
           </li>
         </ul>
         <h3 className="h4-style">Si vous recevez un résultat de test négatif (vous n’avez pas le virus)</h3>
-        <p>Vous pouvez retourner à l’école si tous les critères qui suivent s’appliquent :</p>
+        <p>Vous devriez vous isoler pendant 14 jours, à moins que tous les critères qui suivent s’appliquent :</p>
         <ul>
           <li>vous ne faites pas de fièvre (sans prendre de médicaments)</li>
           <li>
@@ -290,7 +290,7 @@ const resultsSchool = {
           </li>
         </ul>
         <h3 className="h4-style">S’ils reçoivent un résultat de test négatif (ils n’ont pas le virus)</h3>
-        <p>Ils peuvent retourner à l’école si tous les critères qui suivent s’appliquent :</p>
+        <p>Ils devriez vous isoler pendant 14 jours, à moins que tous les critères qui suivent s’appliquent :</p>
         <ul>
           <li>ils ne font pas de fièvre (sans prendre de médicaments)</li>
           <li>
